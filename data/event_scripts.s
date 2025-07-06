0f9cf3bf8940be52c8ca72712d552776d0ffcee9
DaDainDan <lelouchismywaifu@gmail.com>
2025-07-05 21:33:38 -0500

    Porymap: Regi Stuff / Scorched Slab
    The Scorched Slab is now the final location for the Regi questline
    - Adds a lower floor
    - Adds a 3-prong Regi puzzle that requires all three Golems
        - Not functional
    - Adds a dynamic entrance to an encounter with Regigigas
    
    Ancient Tomb / Desert Ruins / Island Cave
    - Swaps the original Regi GFX for the new Overworld GFX
    - Changes the rock and floor formation between them to add variety

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1165,3 +1165,5 @@ EventScript_VsSeekerChargingDone::
 	.include "data/maps/AlteringCave_B1F_2R/scripts.inc"
 
 	.include "data/maps/AlteringCave_B2F/scripts.inc"
+
+	.include "data/maps/ScorchedSlab_B1F/scripts.inc"
