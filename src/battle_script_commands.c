e586236632a9ef396679296c4f1266ed5a33fcd3
DaDainDan <lelouchismywaifu@gmail.com>
2026-09-18 23:07:00 -0500

    Updating pre-1.17.1: commit 2cae00f -> Prevent out-of-bounds battler accesses

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -3596,13 +3596,14 @@ static void Cmd_openpartyscreen(void)
 
         hitmarkerFaintBits = gHitMarker >> 28;
 
-        gBattlerFainted = 0;
-        while (!((1u << gBattlerFainted) & hitmarkerFaintBits)
-               && gBattlerFainted < gBattlersCount)
-            gBattlerFainted++;
+        u32 fainted = 0;
+        while (fainted < gBattlersCount && !((1u << fainted) & hitmarkerFaintBits))
+            fainted++;
 
-        if (gBattlerFainted == gBattlersCount)
+        if (fainted == gBattlersCount)
             gBattlescriptCurrInstr = failInstr;
+        else
+            gBattlerFainted = fainted;
     }
     else
     {
@@ -3781,6 +3782,9 @@ static void Cmd_switchineffects(void)
 static void Cmd_switchinevents(void)
 {
     CMD_ARGS();
+
+    assertf(gBattlerFainted < MAX_BATTLERS_COUNT, "invalid gBattlerFainted: %d", gBattlerFainted);
+
     while (gBattleStruct->eventState.switchIn < SWITCH_IN_EVENTS_COUNT)
     {
         if (DoSwitchInEvents())
@@ -11907,11 +11911,6 @@ void BS_TryTrainerSlideMsgLastOn(void)
 {
     NATIVE_ARGS(u8 battler);
     enum BattlerId battler = GetBattlerForBattleScript(cmd->battler);
-
-    if (battler >= MAX_BATTLERS_COUNT) // Edge case for double KO cases where gBattlerFainted == MAX_BATTLERS_COUNT so GetBattlerForBattleScript returns 6
-    {
-        gBattlescriptCurrInstr = cmd->nextInstr;
-    }
     enum BattlerId tempBattler = gBattleScripting.battler;
 
     switch (gBattleScripting.battler)
